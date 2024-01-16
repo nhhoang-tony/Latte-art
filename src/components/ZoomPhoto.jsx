98fd6e9c63436d0e453c1890b45e327b8d9800e5
Tony Nguyen <t.nguyen@vbreathe.com>
2024-01-16 20:42:47 +1000

    Make zoom photo bigger

--- src/components/ZoomPhoto.jsx
+++ src/components/ZoomPhoto.jsx
@@ -8,13 +8,13 @@ export default function ZoomPhoto({ photoPath, hidePhoto }) {
       {photoPath.includes('jpeg') && (
         <img
           src={`/static/img/${photoPath}`}
-          className='max-w-[700px] max-h-[700px] w-4/5 rounded-3xl z-10 brightness-110'
+          className='max-w-[700px] max-h-[700px] w-[95%] rounded-3xl z-10 brightness-110'
           loading='lazy'
         ></img>
       )}
       {photoPath.includes('mp4') && (
         <video
-          className='max-w-[700px] max-h-[700px] w-4/5 rounded-3xl z-10 brightness-110'
+          className='max-w-[700px] max-h-[700px] w-[95%] rounded-3xl z-10 brightness-110'
           src={`/static/img/${photoPath}`}
           type='video/mp4'
           loop
