4704da5c490c35eafdcdeef8ca226c286a0dcf6a
Tony Nguyen <t.nguyen@vbreathe.com>
2024-01-17 04:44:25 +1000

    Fix zoom photo

--- src/components/ZoomPhoto.jsx
+++ src/components/ZoomPhoto.jsx
@@ -4,17 +4,17 @@ export default function ZoomPhoto({ photoPath, hidePhoto }) {
       className='fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center animation'
       onClick={hidePhoto}
     >
-      <div className='fixed w-full h-full bg-[#0f0e18] opacity-95 z-0'></div>
+      <div className='fixed w-full h-screen bg-[#0f0e18] opacity-95 z-0'></div>
       {photoPath.includes('jpeg') && (
         <img
           src={`/static/img/${photoPath}`}
-          className='max-w-[700px] max-h-[700px] w-[95%] rounded-3xl z-10 brightness-110 bg-[#091712f2]'
+          className='fixed max-w-[700px] max-h-[700px] w-[95%] rounded-3xl z-10 brightness-110 bg-[#091712f2]'
           loading='lazy'
         ></img>
       )}
       {photoPath.includes('mp4') && (
         <video
-          className='max-w-[700px] max-h-[700px] w-[95%] rounded-3xl z-10 brightness-110'
+          className='fixed max-w-[700px] max-h-[700px] w-[95%] rounded-3xl z-10 brightness-110'
           src={`/static/img/${photoPath}`}
           type='video/mp4'
           loop
